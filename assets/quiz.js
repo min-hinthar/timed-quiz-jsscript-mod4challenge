9f743a5156892b8c9c8e29876c52499371e867e0
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-05-22 23:28:41 -0700

    quiz.css container fit content, font-size responsiveness %, box-shawdow 5rem

---
+++ assets/quiz.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//     var $el = $('div > h1') //select all h1
+//         text = $el.text(), //select text values
+//         speed = 1000; //display speed
+//     $el.empty();
+
+//     var wordArray = text.split(' '),
+//         i = 0;
+
+//     INV = setInterval(function () {
+//         if (i >= wordArray.length -1) {
+//             clearInterval(INV);
+//         }
+//         $el.append(wordArray[i] + ' ');
+//         i++;
+//     }, speed);
+// });
