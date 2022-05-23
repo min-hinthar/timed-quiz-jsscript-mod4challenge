93198f5a02b6d62ec4ffb132b76ed910d69c6816
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-05-23 09:01:55 -0700

    quiz.js currentQuestion, availableQuestions, scoreArray added

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,6 +1,33 @@
+const question = document.querySelector('#question');
+const choices = document.querySelectorAll('.choice-text');
+const questionProgress = document.querySelector('#questionNum');
+const scoreText = document.querySelector('#scoreNum');
+
+let availableQuestions = 0;
+let currentQuestion = {};
+let questionCounter = 0;
+let chosenAnswer = true;
+let score = 0;
+
+let question = [
+    {
+        question: "Javascript is an [_______] language?",
+        choice1: 
+    }
+]
 
 
-
+// WHEN I click the start button -->
+// <!-- href to game.html page to start game function -->
+// <!-- THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score -->
 
 
 
