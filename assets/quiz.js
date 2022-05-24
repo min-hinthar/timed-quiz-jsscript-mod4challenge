e9d4b28007711e1f6a4366dffdb29f680e3a55ec
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-05-24 03:49:43 -0700

    quiz.js starGame() in progress error with quiz.css choice-container array undefined

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,7 +1,7 @@
-const question = document.querySelector('#question');
-const choices = document.querySelectorAll('.choice-text');
-const questionProgress = document.querySelector('#questionNum');
-const scoreText = document.querySelector('#scoreNum');
+const question = document.querySelectorAll('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const questionProgress = document.querySelectorAll('#questionNum');
+const scoreText = document.querySelectorAll('#scoreNum');
 
 let availableQuestions = 0;
 let currentQuestion = {};
@@ -9,18 +9,116 @@ let questionCounter = 0;
 let chosenAnswer = true;
 let score = 0;
 
-let question = [
+let questions = [
     {
-        question: "Javascript is an [_______] language?",
-        choice1: 
-    }
+        question: "JavaScript is the programming language of the [______]",
+        choice1: "Desktop",
+        choice2: "Mobile",
+        choice3: "Web",
+        choice4: "Server",
+        answer: 3,
+    },
+    {
+        question: "Which type of JavaScript language is [_____]?",
+        choice1: "Object-oriented",
+        choice2: "Object-based",
+        choice3: "Functional programming",
+        choice4: "All of the above",
+        answer: 2,
+    },
+    {
+        question: "Which of the following statement(s) is true about the JavaScript?",
+        choice1: "It is a scripting language used to make the website interactive",
+        choice2: "It is an advanced version of Java for Desktop and Mobile application development",
+        choice3: "It is a markup language of Java to develop the webpages",
+        choice4: "All of the above",
+        answer: 1,
+    },
+    {
+        question: "In which HTML element, we put the JavaScript code?",
+        choice1: "<javascript>...</javascript>",
+        choice2: "<js>...</js>",
+        choice3: "<script>...</script>",
+        choice4: "<css>...</css>",
+        answer: 3,
+    },
+    {
+        question: "JavaScript code can be written in [_____]",
+        choice1: "JavaScript file (.js file)",
+        choice2: "HTML document directly",
+        choice3: "JavaScript file and in HTML document directly",
+        choice4: "In style sheets (.css file)",
+        answer: 3,
+    },
+    // {
+    //     question: "Which symbol is used separate JavaScript statements?",
+    //     choice1: "Comma (,)",
+    //     choice2: "Colon (:)",
+    //     choice3: "Hyphen (_)",
+    //     choice4: "Semicolon (;)",
+    //     answer: 4,
+    // },
 ]
 
+const scorePoints = 100;
+const maxQuestions = 5;
+// WHEN I click the start button
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    // console.log('Start Quiz!')
+    getNewQuestion()
+} 
 
-// WHEN I click the start button -->
-// <!-- href to game.html page to start game function -->
-// <!-- THEN a timer starts and I am presented with a question
+// THEN a timer starts and I am presented with a question
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > maxQuestions) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/scoreboard.html')
+    }
+
+    // questionCounter++
+    // incrementScore(questionCounter)
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion.question['choice' + number] 
+    } )
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    chosenAnswer = true
+}
+
 // WHEN I answer a question
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!chosenAnswer) return
+        
+        chosenAnswer = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+        
+        if(selectedChoice == true ) {
+            incrementScore(scorePoints)
+        }
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame ()
+
+
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
